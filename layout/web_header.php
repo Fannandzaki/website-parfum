a2dd4041ff416034d68a1c4127cd37214a3faf91
fannan dzaki <frl153071@gmail.com>
2026-04-23 23:03:01 +0700

    update Layout And Theme

--- layout/web_header.php
+++ layout/web_header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Parfumerie - Maroon Company</title>
+    <title>Parfumerie - Nan Company</title>
     <?php require_once './partials/head.php' ?>
 </head>
 <body>
